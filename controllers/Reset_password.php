848b8fa5983f1fe32de3f88ffcecec36a153e668
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2021-12-31 13:48:39 +0530

    commit

--- controllers/Reset_password.php
+++ controllers/Reset_password.php
@@ -9,5 +9,61 @@ class Reset_password extends Controller{
     function index(){
         $this->view->render('reset_password');
     }
-    
+
+    function reset()
+    {
+        require 'config/PathConf.php';
+        
+            
+
+            if (isset($_POST['submit'])) {
+
+                $username = $_POST['username'];
+                $password = $_POST['passowrd'];
+                $repassword = $_POST['repassword'];
+
+                echo $password;
+
+                echo "</br>";
+                echo $repassword;
+
+                if ( empty($password) || empty($repassword)) {
+                    $_SESSION['error'] = "emptyinput";
+                    echo $_SESSION['error'];
+                    echo "</br>";
+                    // header("location:".$localhost."Reset_password");
+                    exit();
+                }
+                else{
+                    if ($password != $repassword) {
+                        $_SESSION['error'] = "passwordmissmatch";
+                        echo $_SESSION['error'];
+                        echo "</br>";
+                        // header("location:".$localhost."Reset_password");
+
+                        exit();
+                    }
+                }
+                $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+                echo $hashedPwd;
+                echo "</br>";
+                $this->model->updatepassword( $hashedPwd , $username);
+                $_SESSION['error'] = "UpdatedSuccess";
+                echo $_SESSION['error'];
+                header("location:".$localhost."Log_in");
+                exit();
+            }
+            // else {
+            //     $_SESSION['error'] = "invalidAccess2";
+            //     header("location:http://localhost/G7/Group07");
+            //     exit();
+            // }
+            
+
+            }
+}
+
+function hello()
+{
+    echo "pavi";
 }
---
+++ views/forget_password.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- <?php
+    session_start();
+?> -->
+<?php require 'config/PathConf.php'; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/9c5a05f882.js" crossorigin="anonymous"></script>
+    <link rel='stylesheet' type='text/css' href="<?php echo $localhost; ?>views/css/forget_password.css">
+    <title>Document</title>
+    
+</head>
+<body>
+<?php
+    $this_page = "forget_password.php";
+    
+?>
+
+<header class="header">
+    <img class="logo" src="assets/logo.png">
+  </header>
+    <div class="container">
+        <div class="container1" id="container1">
+        <h1>Forget Password?</h1>
+        <p>To Verify Your Email Please Enter Your User Name</p>
+       
+        <form action="Forget_password/forget" method="post">
+        <p>
+            <div class="input-section">
+                <label for="username"><i class='bx bxs-user'></i></label>
+                <input type="username" name="username" id="username" class="username" placeholder="User Name" required>
+            </div>    
+            </p>
+            <button type="submit" class="submitbutton" name="submit">Verify</button>
+        </form>
+        </div>
+
+
+        <div class="container2" id="container2">
+        <h1>Verify this is you</h1>
+        <p>To Verify You please enter the verification code in your e-mail</p>
+        <form action="Forget_password/match" method="POST">
+            <p>
+            <div class="input-section">
+                <label for="verfication_code"><i class='bx bx-key' ></i></label>
+                <input type="text" name="code" id="code" placeholder="verify_code"  required>
+            </div>    
+            </p>
+            <button type="submit" class="submitbutton" name="submit">Verify</button>
+        </form>
+        </div>
+    </div>
+    <script type="text/javascript" src="views/js/forget.js"></script>
+</body>
+</html>
