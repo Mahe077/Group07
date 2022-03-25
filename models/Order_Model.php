f4bf5245f548d942ac95c55fe19d1e52fb2ee1be
Mahe076 <maheshlakshan766@gmail.com>
2022-03-25 21:41:46 +0530

    2022-03-25

--- models/Order_Model.php
+++ models/Order_Model.php
@@ -6,16 +6,23 @@ class Order_Model extends Model
         parent::__construct();
     }
 
-    public function loadAll($id,$type)
+    public function loadAll($id, $type)
     {
         return $this->db->select2(
-            "SELECT qty,total_payment,status,amount,name,order_id,ItemId FROM (SELECT * FROM (SELECT * FROM orders,order_item WHERE order_item.OrderId = orders.order_id) tmp ,item WHERE tmp.ItemId = item.id) tmp ,product WHERE (product.id = tmp.productId AND user_id = :id) AND tmp.status =:type ORDER BY tmp.order_id;",
-            ['id' => $id,'type' =>$type]
+            "SELECT * FROM (
+                SELECT order_id,item_id,order_type,total_payment,payment,Qty,amount,price,productId,name,user_id,Cost  FROM (
+                    SELECT order_id,item_id,order_type,total_payment,payment,Qty,amount,price,productId,user_id,Cost  FROM (
+                        SELECT order_id,order_item.itemId as item_id,order_type,total_payment,payment,Qty,user_id,Cost  FROM orders 
+                        INNER JOIN order_item ON order_item.OrderId = orders.order_id) tmp 
+                    INNER JOIN item ON tmp.item_id = item.id) tmp2 
+                INNER JOIN product ON product.id = tmp2.productId) tmp3 
+            WHERE user_id = :id AND order_type= :type ORDER BY order_id ASC",
+            ['id' => $id, 'type' => $type]
         );
     }
     public function Remove($order_id)
     {
-        return $this->db->update("UPDATE `orders` SET `status` = '2' WHERE `orders`.`order_id` = :order_id;", ['order_id' => $order_id]);
+        return $this->db->update("UPDATE `orders` SET `order_type` = '2' WHERE `orders`.`order_id` = :order_id;", ['order_id' => $order_id]);
     }
 
     public function loadAllSpecial($id)
@@ -29,15 +36,17 @@ class Order_Model extends Model
     {
         return $this->db->select2(
             "SELECT `order_id`FROM `orders` WHERE `user_id`=:user_id AND order_id = :order_id;",
-            ['order_id' => $order_id,'user_id' => $_SESSION['userid']]
+            ['order_id' => $order_id, 'user_id' => $_SESSION['userid']]
         );
     }
     public function addReturn($order_id, $reason)
     {
-       return $this->db->transaction(array(
-           array("UPDATE `orders` SET `status`=:status WHERE order_id =:order_id", 
-           ['order_id' => $order_id, 'status' => 5]),
-           array("INSERT INTO `return_orders`(`o_id`, `reason`) VALUES (:order_id,:reason)",['order_id' => $order_id ,'reason' => $reason])
-       ));
+        return $this->db->transaction(array(
+            array(
+                "UPDATE `orders` SET `order_type`=:status WHERE order_id =:order_id",
+                ['order_id' => $order_id, 'status' => 5]
+            ),
+            array("INSERT INTO `return_orders`(`o_id`, `reason`) VALUES (:order_id,:reason)", ['order_id' => $order_id, 'reason' => $reason])
+        ));
     }
 }
