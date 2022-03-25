ec231a11e15798b384e61dc1cd130b4e7024e3e5
Mahe076 <maheshlakshan766@gmail.com>
2022-03-25 21:54:59 +0530

    modifief

--- views/js/customer/Notification_header.js
+++ views/js/customer/Notification_header.js
@@ -2,7 +2,12 @@ function UpdateNotificationIcon() {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost/G7/Group07/Notification/displayNotificationCount");
     xhr.onload = function () {
-      document.querySelector(".fa-bell-span").innerHTML = this.response;
+      if(this.response == 'null'){
+        document.querySelector(".fa-bell-span").innerHTML = 0;
+      }else{
+        document.querySelector(".fa-bell-span").innerHTML = this.response;
+      }
+      
     };
     xhr.send();
     return;
