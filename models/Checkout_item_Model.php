8e349313c1165b0d79badc29f1b98ac8725ad867
kasunikahansamali <kasunikahansamali.96@gmail.com>
2022-03-27 11:06:58 +0530

    commit

--- models/Checkout_item_Model.php
+++ models/Checkout_item_Model.php
@@ -7,7 +7,7 @@ class Checkout_item_Model extends Model
     }
     public function Check_availability($id){
         $item_id=$id;
-        return $this->db->select2("SELECT brand,type,price,size,partNo_manufacturer,partNo,amount,color,image_path from item INNER JOIN itemcolor ON item.id=itemcolor.id INNER JOIN itemimage on itemimage.id=itemcolor.id WHERE item.id = :id;",['id'=>$item_id]);
+        return $this->db->select2("SELECT brand,genuine,price,size,partNo_manufacturer,partNo,amount,color,image_path from item INNER JOIN itemcolor ON item.id=itemcolor.id INNER JOIN itemimage on itemimage.id=itemcolor.id WHERE item.id = :id;",['id'=>$item_id]);
     }
     public function Displaynoti(){
         return  $this->db->select("SELECT count(*)FROM `notification` WHERE status = '0'");
